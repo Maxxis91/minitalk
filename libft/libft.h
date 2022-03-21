919d7e283a31dea38f91ef71815d385350801d15
Gastly Melissia <gmelissi@si-a1.kzn.21-school.ru>
2022-03-21 16:20:56 +0300

    Minitalk

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmelissi <gmelissi@student.21-schoo>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 15:21:06 by gmelissi          #+#    #+#             */
+/*   Updated: 2022/03/19 03:41:05 by gmelissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
